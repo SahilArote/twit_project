7e48ac3ba66891e44cb7e9e2e54a3df36970e8de
SahilArote <arotesahil2@gmail.com>
2025-12-25 21:56:18 +0530

    Normalize line endings

--- swetter/swetter/settings.py
+++ swetter/swetter/settings.py
@@ -75,8 +75,8 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
-
-
+ACCOUNT_LOGIN_METHODS = {"email"}
+ACCOUNT_SIGNUP_FIELDS = ["email*", "password1*", "password2*"]
 
 
 
