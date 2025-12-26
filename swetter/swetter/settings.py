21e98eeb3ebc5e442424c44cb2783e26a9193b19
SahilArote <arotesahil2@gmail.com>
2025-12-26 11:23:09 +0530

    database

--- swetter/swetter/settings.py
+++ swetter/swetter/settings.py
@@ -129,10 +129,9 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-    )
+    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
+
 
 
 
